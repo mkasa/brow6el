858dd9d30e596fcad2d92f40591aa1ac52c247d9
Jan Antos <jan@janantos.com>
2026-01-14 18:22:04 +0200

    switch to appimagetool instead linuxdeploy

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 echo "Compiling..."
 make -j$(nproc)
 
-#echo "Stripping debug symbols from libcef.so..."
-#if [ -f "libcef.so" ]; then
-#    strip --strip-debug libcef.so
-#    echo "libcef.so stripped (debug symbols removed)"
-#fi
+echo "Stripping debug symbols from libcef.so..."
+if [ -f "libcef.so" ]; then
+    strip --strip-debug libcef.so
+    echo "libcef.so stripped (debug symbols removed)"
+fi
 
 echo ""
 echo "Build complete!"
